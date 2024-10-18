91c0d7343e4c035fe6928ef93f93b6e3a5514b2b
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-18 22:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    let arr = [
+        { link: "https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", text: "Explore the world", id: "panel-1" },
+        { link: "https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", text: "Wild Forest", id: "panel-2" },
+        { link: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80", text: "Sunny Beach", id: "panel-3" },
+        { link: "https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80", text: "City on Winter", id: "panel-4" },
+        { link: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", text: "Mountain Cloud", id: "panel-5" }
+    ]
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="styles.css">
-</head>
+    let container = document.createElement("div")
+    container.className = "container"
+    document.body.append(container);
 
-<body>
-	<!-- the entire body must be written by student -->
-	<script type="text/javascript" src="./script.js">
-	</script>
-</body>
-</html>
+    arr.forEach((t, i) => {
+        let panel = document.createElement("div")
+        panel.classList.add("panel")
+        panel.id = t.id
+        panel.innerHTML = 
+        `
+            <img src="${t.link}" alt="">
+            <p class="img-text">${t.text}</p>
+        `;
+        container.append(panel);
+
+        if (i === 0) {
+            panel.classList.add("active")
+        }
+
+        panel.addEventListener("click", () => {
+            removeActive();
+            panel.classList.add("active");
+            console.log(panel);
+        });
+        
+    });
+    
+    console.log(document.querySelectorAll(".panel"));
+    function removeActive() {
+        document.querySelectorAll(".panel").forEach(panel => {
+            panel.classList.remove("active");
+        });
+    }
+
+
+
+})
